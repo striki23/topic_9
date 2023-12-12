4fd419e0cbbf30b5dfeb00c6ae4df941a597a635
Dasha Demidova <striki06@mail.ru>
2023-12-12 14:11:47 +0300

    upgrade

--- tasks/part_2_insightful/task_5.py
+++ tasks/part_2_insightful/task_5.py
@@ -1,8 +1,14 @@
-# Программа заменяе все гласные в словах на знак '?'
-line: str = input()
+# Программа заменяет все гласные в словах на знак '?'
+line: str = input().lstrip()
 
 vowels: str = 'AaEeIiOoUuАаИиОоУуЫыЭэЁёЕеЮюЯя'
+symbol_swap: str = '?' * 30
+tbl: dict = line.maketrans(vowels, symbol_swap)
+print(line.translate(tbl))
 
-for symbol in vowels:
-    line = line.replace(symbol, '?')
-print(line)
+
+# vowels: str = 'AaEeIiOoUuАаИиОоУуЫыЭэЁёЕеЮюЯя'
+#
+# for symbol in vowels:
+#     line = line.replace(symbol, '?')
+# print(line)
--- tasks/part_2_insightful/task_6.py
+++ tasks/part_2_insightful/task_6.py
@@ -2,12 +2,24 @@
 # букв, цифр и пробелов в введенной строке line
 line: str = input()
 
-# заносим в список letters все элементы строки, которые являются буквой
-letters: list[str, ...] = [i for i in line if i.isalpha()]
-# заносим в список digit все элементы строки, которые являются цифрой
-digits: list[str, ...] = [i for i in line if i.isnumeric()]
-# заносим в список space все элементы строки, которые являются пробелом
-spaces: list[str, ...] = [i for i in line if i.isspace()]
+# # заносим в список letters все элементы строки, которые являются буквой
+# letters: list[str, ...] = [i for i in line if i.isalpha()]
+# # заносим в список digit все элементы строки, которые являются цифрой
+# digits: list[str, ...] = [i for i in line if i.isnumeric()]
+# # заносим в список space все элементы строки, которые являются пробелом
+# spaces: list[str, ...] = [i for i in line if i.isspace()]
+#
+# # выводим кол-во букв и цифр, пробелов
+# print(len(letters), len(digits), len(spaces), sep='\n')
 
-# выводим кол-во букв и цифр, пробелов
-print(len(letters), len(digits), len(spaces), sep='\n')
+letters, digits, spaces = (0, 0, 0)
+
+for el in line:
+    if el.isalpha():
+        letters += 1
+    elif el.isnumeric():
+        digits += 1
+    elif el.isspace():
+        spaces += 1
+
+print(letters, digits, spaces, sep='\n')
